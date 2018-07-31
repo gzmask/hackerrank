62faedf56a6e8e7099ef22d488a15873c4fb4b69
Shulang Lei <shulang.lei@unbounce.com>
2018-07-30 20:55:20 -0700

    Added problem link.

--- balance/src/hackerrack/core.clj
+++ balance/src/hackerrack/core.clj
@@ -1,3 +1,4 @@
+;; https://www.hackerrank.com/challenges/ctci-balanced-brackets/problem
 (ns hackerrack.core)
 
 (def paras {\{ \}
--- count-swap/src/count_swap/core.clj
+++ count-swap/src/count_swap/core.clj
@@ -1,3 +1,4 @@
+;; https://www.hackerrank.com/challenges/ctci-bubble-sort/problem
 (ns count-swap.core)
 
 (defn swapper [swap-count a [i1 i2]]
